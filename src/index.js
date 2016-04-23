4c2f055a88dd42b96e7f654b76b8d42346c0bd69
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-04-23 19:44:09 -0400

    added SearchBar comoponent

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar.js';
+
 const API_KEY = 'AIzaSyCnCdch8E15oFsUMQPiCwgOZs8QVXE-JqM';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
