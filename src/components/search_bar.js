e8ceb94ac5669a805067e06c313f2ca768c3e170
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-04-24 20:30:38 -0400

    implemented video list, video detail and searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,14 +8,17 @@ class SearchBar extends Component {
     this.state = { term: '' };
   }
 
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value });
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 
   render() {
     return (
-      <div>
-        <input value={this.state.term} onChange={this.onInputChange} />
+      <div className="search-bar">
+        <input
+        value={this.state.term}
+        onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
   }
